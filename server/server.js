63f5d83746e176a1d9caa361b297763dc61449d2
Ignacio <nachodiaz99@gmail.com>
2022-04-05 19:02:03 +0300

    attempt n 999

--- server/server.js
+++ server/server.js
@@ -10,8 +10,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
-
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('../client/build'));
 }
@@ -19,6 +17,8 @@ if(process.env.NODE_ENV === 'production'){
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
+
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false})
