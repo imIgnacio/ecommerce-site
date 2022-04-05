670ac27a0be56389f1bf7e0afa6dc8af65f52415
Ignacio <nachodiaz99@gmail.com>
2022-04-05 14:25:25 +0300

    testing deploy 2

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ router.use('/api', apiRoutes);
 
 // All other GET requests not handled before will return our React app
 router.use('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
 });
 
 module.exports = router;
