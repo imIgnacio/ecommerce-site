dfcb4f1613f7f3e1798d8df71b3208110c7421e6
Ignacio <nachodiaz99@gmail.com>
2022-04-05 19:08:04 +0300

    keep going

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req,res) => {
