1dff15985e817f868e4bae8e8c4e86e6161b6b6b
Ignacio <nachodiaz99@gmail.com>
2022-04-05 19:22:06 +0300

    I think this can go to production

--- server/server.js
+++ server/server.js
@@ -8,17 +8,17 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.use(routes);
+
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
-
-app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false})
