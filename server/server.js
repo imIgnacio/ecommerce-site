1d66d6e04b3389a88fe25e3e92c3c6c4227e9ee6
Ignacio <nachodiaz99@gmail.com>
2022-04-05 13:58:49 +0300

    deploying to heroku

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(routes);
 
