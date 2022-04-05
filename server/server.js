779d3c2e3bd3f7b9a402246aae2b548fc2fde870
Ignacio <nachodiaz99@gmail.com>
2022-04-05 18:30:19 +0300

    fixing typo

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(routes);
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('../client/build'));
   app.get('*', (req,res) => {
-    req.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   })
 }
 
