28c5383d4021a5dd212de94d7af2923497792843
Ignacio <nachodiaz99@gmail.com>
2022-05-03 15:58:17 -0400

    test

--- server/seeds/product-seeds.js
+++ server/seeds/product-seeds.js
@@ -6,14 +6,14 @@ const productData = [
     price: 14.99,
     stock: 14,
     category_id: 1,
-    image: 'familiaPetLogo.JPG',
+    image: 'zootec.JPG',
   },
   {
     product_name: 'Running Sneakers',
     price: 90.0,
     stock: 25,
     category_id: 5,
-    image: 'https://www.childrensalonoutlet.com/media/catalog/product/cache/0/small_image/256x256/9df78eab33525d08d6e5fb8d27136e95/m/a/mayoral-boys-blue-leather-trainers-403935-9526967c1a13aa4c758f0b052268e6d38205dd60.jpg',
+    image: 'petMatte.JPG',
   },
   {
     product_name: 'Branded Baseball Hat',
